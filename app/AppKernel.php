71ffee3561ddd27f58127d2fb5773a1bc046e1e2
kiperz <kiperz@gmail.com>
2017-02-09 15:57:47 +0100

    rename fixes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new AppBundle\AppBundle(),
+            new FrontBundle\FrontBundle(),
+            new BackOfficeBundle\BackOfficeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
